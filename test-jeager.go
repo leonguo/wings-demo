dc6c7829113c1fae3fcb5737519458e687280da8
wings <hotman8168@gmail.com>
2017-10-11 19:26:39 +0800

    add http

--- test-jeager.go
+++ test-jeager.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"github.com/leonguo/wings/tracing"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"io"
 )
 
@@ -14,38 +13,10 @@ type data struct {
 	tracer opentracing.Tracer
 }
 
-type TracedServeMux1 struct {
-	mux    *http.ServeMux
-	tracer opentracing.Tracer
-}
-
-// NewServeMux creates a new TracedServeMux.
-func NewServeMux1(tracer opentracing.Tracer) *TracedServeMux1 {
-	return &TracedServeMux1{
-		mux:    http.NewServeMux(),
-		tracer: tracer,
-	}
-}
-
-// Handle implements http.ServeMux#Handle
-func (tm *TracedServeMux1) Handle(pattern string, handler http.Handler) {
-	middleware := nethttp.Middleware(
-		tm.tracer,
-		handler,
-		nethttp.OperationNameFunc(func(r *http.Request) string {
-			return "HTTP " + r.Method + " " + pattern
-		}))
-	tm.mux.Handle(pattern, middleware)
-}
-
-// ServeHTTP implements http.ServeMux#ServeHTTP
-func (tm *TracedServeMux1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	tm.mux.ServeHTTP(w, r)
-}
 func main() {
 	tracer := tracing.InitTrace(app_name)
 	d := &data{tracer: tracer}
-	m := NewServeMux1(d.tracer)
+	m := tracing.NewServeMux(d.tracer)
 	m.Handle("/", http.HandlerFunc(d.hello))
 	m.Handle("/test", http.HandlerFunc(d.test))
 	http.ListenAndServe(":9000", m.mux)
